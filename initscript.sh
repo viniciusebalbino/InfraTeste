cfa8e232ba6470b16be07d3c9d464370bacedb19
Vinicius Balbino <viniciusbalbino@icloud.com>
2020-07-20 12:17:35 -0300

    Correções no docker-compose.yaml

--- initscript.sh
+++ initscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#Instalando Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+#Iniciando os containers via Docker-compose
+cd webapp && docker-compose up -d
